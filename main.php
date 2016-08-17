4ab26ab7a33ed4abb54cb1ab5a3847a461761487
tudders <sam@samtudman.onmicrosoft.com>
2016-08-17 21:26:59 +1000

    Add files via upload

---
+++ main.php
@@ -0,0 +1,28 @@
+<H1> SQL CONNECTION TEST </H1>
+
+<?php
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+echo time()."<br>";
+
+$serverName = "accounting-test.database.windows.net";
+$connectionOptions = array("Database"=>"accounting-test",
+                                    "Uid" => "tudders",
+                                    "PWD"=>"Ioio1010"
+                                    );
+
+$conn = sqlsrv_connect($serverName,$connectionOptions);
+
+if($conn===FALSE){
+    echo ("connection failed");
+}
+else {
+    echo ("connection succeded");
+}
+
+
+?>
+
+
